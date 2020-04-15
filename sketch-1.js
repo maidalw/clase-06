5a707a21f6b5e8fc4028736c49a8eeb101cb8428
maidalw <49153855+maidalw@users.noreply.github.com>
2020-04-15 10:46:12 -0400

    Add files via upload

---
+++ sketch-1.js
@@ -0,0 +1,19 @@
+var a = 50;
+var c;
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  colorMode(HSB, 360, 100, 100);
+  //background(0,100,100);
+  fill(random(0,40),100,100);
+  noStroke();
+  c = a * random (1,5);
+  ellipse(mouseX,mouseY,c,c);
+  /*
+  comentario
+  más
+  largo
+  */
+}
